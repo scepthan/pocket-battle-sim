fd55ebc8afc7a86fa3cb28fee4118e960bfeb5b9
scepthan <scepthan@gmail.com>
2025-01-16 19:05:11 -0500

    Basic npm install

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-app>
+    <v-main> Hello world! </v-main>
+  </v-app>
+</template>
+
+<script setup lang="ts">
+import cards from "./assets/cards.json";
+
+console.log(cards);
+</script>
