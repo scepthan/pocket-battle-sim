74e67333d22b5ec2b36fb9a5fccbff7756f7f89d
scepthan <scepthan@gmail.com>
2025-09-19 16:17:15 -0400

    Make BetterRandomAgent slightly less random
    Will now pick the last attack on the card whenever possible rather than picking randomly.
    There are definitely more factors that could be taken into consideration; for example, I would not expect Decidueye ex to do well with this bot.
    It's a marginal improvement over the last version (52.3% winrate) but it does dramatically better with most decks that have mon with 2 attacks.
    For example, A1 Solo Battles - Charizard ex & Moltres ex went from 44% to 56% winrate.

--- src/core/agents/BetterRandomAgent/index.ts
+++ src/core/agents/BetterRandomAgent/index.ts
@@ -1,3 +1,4 @@
 export { BetterRandomAgent } from "./BetterRandomAgent";
 export { BetterRandomAgent_v0_1_0 } from "./BetterRandomAgent_v0_1_0";
 export { BetterRandomAgent_v0_1_1 } from "./BetterRandomAgent_v0_1_1";
+export { BetterRandomAgent_v0_1_2 } from "./BetterRandomAgent_v0_1_2";
