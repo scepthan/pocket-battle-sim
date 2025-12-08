618af2f0ca800c793bee662dd3eb344e8969620e
scepthan <scepthan@gmail.com>
2025-12-08 17:13:35 -0500

    Add A2b solo battle decks

--- src/assets/decks/index.ts
+++ src/assets/decks/index.ts
@@ -3,11 +3,12 @@ import A1 from "./A1.json";
 import A1a from "./A1a.json";
 import A2 from "./A2.json";
 import A2a from "./A2a.json";
+import A2b from "./A2b.json";
 
 export type DeckList = Record<string, DeckInfo>;
 export type DeckRecord = Record<string, DeckList>;
 
-export const decks = { ...A1, ...A1a, ...A2, ...A2a } as DeckRecord;
+export const decks = { ...A1, ...A1a, ...A2, ...A2a, ...A2b } as DeckRecord;
 
 export const allDecks = Object.entries(decks).reduce((acc, [setName, deckList]) => {
   Object.entries(deckList).forEach(([deckName, deckInfo]) => {
