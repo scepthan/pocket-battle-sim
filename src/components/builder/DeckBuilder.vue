13c13ddac74cbcca02399814772ddd1ff7fdeada
scepthan <scepthan@gmail.com>
2026-01-08 14:23:42 -0500

    Automatically detect required energy types in deck builder

--- src/components/builder/DeckBuilder.vue
+++ src/components/builder/DeckBuilder.vue
@@ -12,7 +12,11 @@
           />
         </v-col>
         <v-col cols="8" class="d-flex ga-2 align-center">
-          <EnergySelector v-model="energyTypes" />
+          <EnergySelector
+            v-model="energyTypes"
+            :set-manually="customEnergySelected"
+            @reset="resetEnergy"
+          />
           <ResetDeckButton @reset="resetDeck" />
           <CopyJsonButton
             :deck="{ Cards: selectedCards.map((x) => x.ID), EnergyTypes: energyTypes }"
@@ -37,7 +41,7 @@
 </template>
 
 <script setup lang="ts">
-import { type DeckInfo, type Energy, type PlayingCard } from "@/core";
+import { EnergyMap, sortedBy, type DeckInfo, type Energy, type PlayingCard } from "@/core";
 import { useDeckStore, usePlayingCardStore } from "@/stores";
 
 export interface Props {
@@ -67,16 +71,54 @@ if (deckName.value === "") {
   deckName.value = `New Deck ${newDeckNumber}`;
 }
 const energyTypes = ref<Energy[]>(props.initialDeck.EnergyTypes.slice());
+const customEnergySelected = ref<boolean>(props.initialDeck.Cards.length > 0);
+const computedEnergy = computed<Set<Energy>>(() => {
+  const requiredEnergyTypes = new Set<Energy>();
+  selectedCards.value.forEach((card) => {
+    if (card.CardType !== "Pokemon") return;
+    card.Attacks.forEach((attack) =>
+      attack.requiredEnergy.forEach((energy) => {
+        if (energy !== "Colorless" && requiredEnergyTypes.size < 3) requiredEnergyTypes.add(energy);
+      })
+    );
+  });
+  requiredEnergyTypes.delete("Colorless");
+  if (requiredEnergyTypes.size === 0) {
+    return new Set(["Grass"]);
+  }
+  return requiredEnergyTypes;
+});
+// Update energy types when selected cards change, unless user has set custom energy
+watch(
+  computedEnergy,
+  (newVal) => {
+    if (!customEnergySelected.value) {
+      energyTypes.value = Array.from(newVal);
+    }
+  },
+  { immediate: true }
+);
+// Mark as custom if selected energy types differ from computed energy
+watch(energyTypes, () => {
+  if (energyTypes.value.toString() !== Array.from(computedEnergy.value).toString()) {
+    customEnergySelected.value = true;
+  }
+});
+const resetEnergy = async () => {
+  energyTypes.value = Array.from(computedEnergy.value);
+  customEnergySelected.value = false;
+};
 
 const deck = computed<DeckInfo>(() => ({
   Cards: selectedCards.value.map((card) => card.ID),
-  EnergyTypes: energyTypes.value.slice(),
+  EnergyTypes: sortedBy(energyTypes.value, (x) => x, Object.values(EnergyMap)),
 }));
 
 const resetDeck = () => {
   selectedCards.value = [];
   deckName.value = "";
   energyTypes.value = ["Grass"];
+  customEnergySelected.value = false;
 };
 </script>
 
--- src/components/builder/EnergySelector.vue
+++ src/components/builder/EnergySelector.vue
@@ -1,10 +1,14 @@
 <template>
   <v-dialog v-model="energyDialog" max-width="380px">
     <template #activator="{ props }">
-      <v-btn v-bind="props" class="rounded-pill">
+      <v-btn
+        v-bind="props"
+        class="rounded-pill"
+        :color="setManually === false ? 'pink-lighten-4' : ''"
+      >
         <div class="d-flex ga-2">
           <EnergyIcon
-            v-for="(type, index) in energyTypes"
+            v-for="(type, index) in energyDisplay"
             :key="index"
             :energy="type"
             width="24px"
@@ -14,7 +18,11 @@
     </template>
 
     <v-card>
-      <v-card-title>Select Energy Types (up to 3)</v-card-title>
+      <v-card-title class="d-flex align-center w-100">
+        <span>Select Energy Types (up to 3)</span>
+        <v-spacer />
+        <v-btn icon="mdi-restore" variant="flat" @click="emit('reset')" />
+      </v-card-title>
 
       <v-card-text>
         <v-chip-group v-model="energyTypes" mandatory multiple column :max="3">
@@ -22,7 +30,7 @@
             v-for="type in filteredEnergyTypes"
             :key="type"
             :value="type"
-            :color="energyTypes?.includes(type) ? 'purple' : ''"
+            :color="energyTypes.includes(type) ? 'purple' : ''"
             style="width: 150px"
             class="justify-center"
           >
@@ -37,6 +45,9 @@
       </v-card-text>
 
       <v-card-actions>
+        <span v-if="setManually !== undefined" class="pl-4">
+          Selected {{ setManually ? "manually" : "automatically" }}
+        </span>
         <v-spacer />
         <v-btn @click="energyDialog = false">Close</v-btn>
       </v-card-actions>
@@ -45,13 +56,23 @@
 </template>
 
 <script setup lang="ts">
-import { type Energy, EnergyMap } from "@/core";
+import { type Energy, EnergyMap, sortedBy } from "@/core";
 
 const energyDialog = ref(false);
 
 const energyTypes = defineModel<Energy[]>("modelValue", {
   default: () => ["Grass"],
 });
+defineProps<{
+  setManually?: boolean;
+}>();
+const emit = defineEmits<{
+  (e: "reset"): void;
+}>();
+
+const energyDisplay = computed(() =>
+  sortedBy(energyTypes.value, (x) => x, Object.values(EnergyMap))
+);
 
 const filteredEnergyTypes = computed(
   () => Object.values(EnergyMap).filter((x) => !["Colorless", "Dragon"].includes(x)) as Energy[]
